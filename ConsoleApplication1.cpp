9b2fa0a38c6e88ec5cedfed21b91c2ea7233c4f8
陳奕劭 <yishao2004@gmail.com>
2023-06-27 06:10:42 +0800

    6/27

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -7,70 +7,103 @@
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
 
-float WINDOW_HEIGHT = 512;
-float WINDOW_WIDTH = 512;
+unsigned int WINDOW_HEIGHT = 512;
+unsigned int WINDOW_WIDTH = 512;
 
-int main(){
-    sf::RenderWindow window(sf::VideoMode((unsigned int)WINDOW_WIDTH, (unsigned int)WINDOW_HEIGHT), "SFML Window");
-    window.setFramerateLimit(60);
-    sf::Clock clock;
-    float movementSpeed = 10;
+class Player {
+private:
+    sf::RectangleShape rectangle;
+    sf::Vector2f pos;
+    bool isJumping;
+    sf::RenderWindow* window;
 
-    sf::RectangleShape PlayerA(sf::Vector2f(100, 200));
+    float movementSpeed = 500.f;
+    float jumpSpeed = -500.f;
+    float gravity = 98.f;
 
-    PlayerA.setFillColor(sf::Color::Red);
-    PlayerA.setPosition(sf::Vector2f(0, 0));
+public:
+    Player(sf::Vector2f size, sf::Vector2f position, sf::Color color, sf::RenderWindow* window)
+        : rectangle(size), isJumping(false){
+        rectangle.setFillColor(color);
+        rectangle.setPosition(position.x, position.y - size.y);
+        this->window = window;
+    }
 
-
-    sf::Font open_sans;
-    open_sans.loadFromFile("C:\\Users\\Administrator\\Desktop\\Visual Studio\\Fonts\\open-sans\\OpenSans-Light.ttf");
-    sf::Text message;
-    message.setFont(open_sans);
-    message.setFillColor(sf::Color::Green);
-    message.setCharacterSize(48);
-    message.setString("Hello World");
-    sf::FloatRect messageBounds = message.getLocalBounds();
-    message.setOrigin(messageBounds.left + messageBounds.width / 2.0f, 0);
-    message.setPosition(window.getSize().x / 2.0f, 0);
-
-    while (window.isOpen()){
-        sf::Event event;
-        
-        while ( window.pollEvent(event) ){
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
-                window.close();
-            }
-        }
-
-        float movementX = 0.0f;
-        float movementY = 0.0f;
+    void update(float deltaTime) {
+        sf::Vector2f velocity = { 0.f, 0.f };
 
         // Check for keyboard input
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-            movementY -= movementSpeed;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-            movementY += movementSpeed;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-            movementX -= movementSpeed;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-            movementX += movementSpeed;
-
-        // Normalize the movement vector if diagonal movement is detected
-        if (movementX * movementY != 0.0f){
-            float length = std::sqrt(movementX * movementX + movementY * movementY);
-            movementX = (movementX / length) * movementSpeed;
-            movementY = (movementY / length) * movementSpeed;
+        if (!isJumping) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+                velocity.x -= movementSpeed;
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+                velocity.x += movementSpeed;
+            }
         }
 
-        PlayerA.move(movementX, movementY); // Move the object
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && !isJumping) {
+            isJumping = true;
+            velocity.y = jumpSpeed;
+        }
+
+        if (isJumping) {
+            velocity.y += gravity * deltaTime;
+
+            if (pos.y>=this->window->getSize().y - rectangle.getSize().y) {
+                velocity.y = 0.0f;
+                isJumping = false;
+            }
+        }
+
+        // Update player position
+        pos = rectangle.getPosition() + sf::Vector2f(velocity.x * deltaTime, velocity.y);
+
+        // Check collision with window borders
+        if (pos.x < 0) {
+            pos.x = 0;
+        }
+        else if (pos.x + rectangle.getSize().x > window->getSize().x) {
+            pos.x = window->getSize().x - rectangle.getSize().x;
+        }
+
+        rectangle.setPosition(pos);
+    }
+
+    void draw(sf::RenderWindow& window) {
+        window.draw(rectangle);
+    }
+};
+
+
+int main() {
+    sf::VideoMode desktopMode = sf::VideoMode::getDesktopMode();
+    sf::RenderWindow window(desktopMode, "SFML Game");
+    window.setPosition(sf::Vector2i(0, 0));
+    window.setFramerateLimit(60);
+    sf::View view(sf::FloatRect(0.f, 0.f, static_cast<float>(desktopMode.width), static_cast<float>(desktopMode.height)));
+    window.setView(view);
+
+    sf::Clock clock;
+
+    Player PlayerA(sf::Vector2f(100, 100), sf::Vector2f(0, (float)window.getSize().y), sf::Color::Red, &window);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+                window.close();
+        }
+
+        float deltaTime = clock.restart().asSeconds();
+        PlayerA.update(deltaTime);
 
         window.clear();
-        window.draw(PlayerA);
-        window.draw(message);
+        PlayerA.draw(window);
         window.display();
     }
+
     return 0;
 }
